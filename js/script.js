e11eeb822c68e03b15c3f0c33a3e31197d0c2525
Ronal Rajesh <ronalrajesh@outlook.com>
2025-03-07 03:16:44 -0600

    Couldn't get password script to work, so tried one from stack overflow

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-<input type="password" id="password" placeholder="Enter Password">
-<button type="button" onclick="togglePassword()">Show</button>
-
-<script>
-  function togglePassword() {
-    let passwordField = document.getElementById("password");
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-    } else {
-      passwordField.type = "password";
+function toggle(button) {
+    var password = document.getElementById("password");
+    if (password.type == "password") {
+        button.innerHTML = "Hide";
+        password.type = "text";
     }
-  }
-</script>
+    else {
+        button.innerHTML = "Show";
+        password.type = "password";
+    }
+}
+
+/* Code snippet from: https://stackoverflow.com/questions/43390966/javascript-show-hide-toggle-button-for-password-field */
