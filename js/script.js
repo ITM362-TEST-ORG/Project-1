107c00529edf082fe1f60bc9cecad3f931ad33ef
Ronal Rajesh <ronalrajesh@outlook.com>
2025-04-04 02:09:24 -0500

    Added CSS for Tablets and changed button text

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function toggle(button) {
     var password = document.getElementById("password");
     if (password.type == "password") {
-        button.innerHTML = "Hide";
+        button.innerHTML = "Hide Password";
         password.type = "text";
     }
     else {
-        button.innerHTML = "Show";
+        button.innerHTML = "Show Password";
         password.type = "password";
     }
 }
